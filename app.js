11f54e2e0213194d9c8f50660522a220608d7b64
85Jakob <jacobadoney@gmail.com>
2022-08-08 17:50:27 -0500

    added reactions to buttons

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-let sum = ( a, b ) => a + b;
+const resultsDisplay = document.querySelector('.title');
+const displayPlayerPick = document.querySelector('.playerPick');
+let playerSelection;
+let current = true;
 
-console.log( sum(10, 2));
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        resultsDisplay.innerHTML = 'TIE';
+    }
+
+}
+
+const rock = document.getElementById('rockBtn');
+const paper = document.getElementById('paperBtn');
+const scissors = document.getElementById('scissorsBtn');
+
+rock.addEventListener('click', ()=>handleClick('ROCK'));
+paper.addEventListener('click', ()=>handleClick('PAPER'));
+scissors.addEventListener('click', ()=>handleClick('SCISSORS'));
+
+function handleClick(selection){
+    if(current === true){
+        playerSelection = selection;
+        displayPlayerPick.innerHTML = selection
+        current = false;
+    }
+
+}
