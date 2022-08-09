cfd1db375f220bb2c7eed4c270ff890664887bb9
85Jakob <jacobadoney@gmail.com>
2022-08-09 08:52:44 -0500

    added maxScore

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const resultsDisplay = document.querySelector('.title');
 const displayPlayerPick = document.querySelector('.playerPick');
 const displayCompPick = document.querySelector('.compPick');
+const rock = document.getElementById('rockBtn');
+const paper = document.getElementById('paperBtn');
+const scissors = document.getElementById('scissorsBtn');
+const cScore = document.querySelector('.cScore');
+const pScore = document.querySelector('.pScore');
 
-let current = true;
+const maxScore = 5;
+let playerScore = 0;
+let computerScore = 0;
+
+rock.addEventListener('click', ()=>handleClick('ROCK'));
+paper.addEventListener('click', ()=>handleClick('PAPER'));
+scissors.addEventListener('click', ()=>handleClick('SCISSORS'));
 
 
 function playRound(playerSelection, computerSelection){
@@ -11,32 +22,39 @@ function playRound(playerSelection, computerSelection){
     }
     else if(playerSelection === 'ROCK' && computerSelection === 'PAPER'){
         resultsDisplay.innerHTML = 'YOU LOSE';
+        computerScore++;
     }
     else if(playerSelection === 'ROCK' && computerSelection === 'SCISSORS'){
         resultsDisplay.innerHTML = 'YOU WIN';
+        playerScore++;
     }
     else if(playerSelection === 'SCISSORS' && computerSelection === 'PAPER'){
         resultsDisplay.innerHTML = 'YOU WIN'
+        playerScore++;
     }
     else if(playerSelection === 'SCISSORS' && computerSelection === 'ROCK'){
         resultsDisplay.innerHTML = 'YOU LOSE'
+        computerScore++;
     }
     else if(playerSelection === 'PAPER' && computerSelection === 'ROCK'){
         resultsDisplay.innerHTML = 'YOU WIN'
+        playerScore++;
     }
     else if(playerSelection === 'PAPER' && computerSelection === 'SCISSORS'){
         resultsDisplay.innerHTML = 'YOU LOSE'
+        computerScore++;
+    }
+    pScore.innerHTML = playerScore;
+    cScore.innerHTML = computerScore;
+
+    if (playerScore === 5){
+        resultsDisplay.innerHTML = 'CONGRATS YOU BEAT THE MACHINE, YOU WIN!!'
+    }
+    else if(computerScore === 5){
+        resultsDisplay.innerHTML = 'YOU LOSE, THE MACHINE HAS WON!! :('
     }
 
 }
-
-const rock = document.getElementById('rockBtn');
-const paper = document.getElementById('paperBtn');
-const scissors = document.getElementById('scissorsBtn');
-
-rock.addEventListener('click', ()=>handleClick('ROCK'));
-paper.addEventListener('click', ()=>handleClick('PAPER'));
-scissors.addEventListener('click', ()=>handleClick('SCISSORS'));
 
 function getRandomChoice() {
     return Math.floor(Math.random() * 3);
@@ -61,11 +79,13 @@ function compChoice(){
 function handleClick(selection){
     let playerSelection;
     let computerSelection;
-    if(current === true){
+
+    if(playerScore < maxScore && computerScore < maxScore){
         playerSelection = selection;
         displayPlayerPick.innerHTML = selection;
         current = false;
         computerSelection = compChoice();
+        
+        playRound(playerSelection, computerSelection);
     }
-    playRound(playerSelection, computerSelection);
 }
